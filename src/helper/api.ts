71aa7c2256b7d9b16696d22375e387c0fc1758a0
toniebe <toniebe48@gmail.com>
2026-04-03 01:25:28 +0700

    HL-010:[fix] add token in inseptor axios

--- src/helper/api.ts
+++ src/helper/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { API_BASE_URL, API_TIMEOUT } from '@env';
+import { authStore } from '../store/authStore';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
@@ -12,7 +13,10 @@ const api = axios.create({
 
 api.interceptors.request.use(
   config => {
-      
+    const token = authStore.getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
   error => Promise.reject(error),
